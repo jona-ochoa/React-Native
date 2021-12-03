25e5003c7beb4d5a63a02bbfba9d4ed7aca8962a
jona-ochoa <jonatan.c.ochoa@gmail.com>
2021-12-03 11:16:38 -0300

    add firebase

--- database/firebase.js
+++ database/firebase.js
@@ -1,21 +1,21 @@
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCWVPa16GB02ealKvJ1dS_vpjeum3vI_Ng",
-//   authDomain: "react-native-firebase-6e5b7.firebaseapp.com",
-//   projectId: "react-native-firebase-6e5b7",
-//   storageBucket: "react-native-firebase-6e5b7.appspot.com",
-//   messagingSenderId: "1020039088178",
-//   appId: "1:1020039088178:web:40d2f21e32c7969f86942b"
-// };
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCWVPa16GB02ealKvJ1dS_vpjeum3vI_Ng",
+  authDomain: "react-native-firebase-6e5b7.firebaseapp.com",
+  projectId: "react-native-firebase-6e5b7",
+  storageBucket: "react-native-firebase-6e5b7.appspot.com",
+  messagingSenderId: "1020039088178",
+  appId: "1:1020039088178:web:40d2f21e32c7969f86942b"
+};
 
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
-// export default {
-//     initializeApp
-// }
+export default {
+    initializeApp
+}
